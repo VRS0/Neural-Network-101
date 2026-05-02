77b8e577c6ec994902f256d253b1deb1ca86fbb1
VRS0 <hawk230a@gmail.com>
2026-05-03 01:42:37 +0300

    updated

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn.datasets import load_iris
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from Architecture import nn
+from architecture import nn
 
 st.title("Iris Flower Classifier")
 st.write("Neural Network Based ~ scratch.")
